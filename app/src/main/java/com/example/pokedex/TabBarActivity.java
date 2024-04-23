c5c8be1ef905f9db6d7beef1d238e7f0b6a82682
svdani <dani.hockey.88@gmail.com>
2024-04-23 19:29:21 +0200

    Creacion Tabbar y fragments

--- app/src/main/java/com/example/pokedex/TabBarActivity.java
+++ app/src/main/java/com/example/pokedex/TabBarActivity.java
@@ -7,6 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class TabBarActivity extends AppCompatActivity {
 
@@ -15,10 +24,45 @@ public class TabBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tab_bar);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+// Obtener TabLayout y ViewPager desde el layout
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+
+        // Configurar el adaptador de fragments y el ViewPager
+        viewPager.setAdapter(new ViewPagerFragmentAdapter(this));
+
+        // Conectar el TabLayout con el ViewPager
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText("Tab " + (position + 1))
+        ).attach();
+    }
+
+
+    private static class ViewPagerFragmentAdapter extends FragmentStateAdapter {
+
+        public ViewPagerFragmentAdapter(AppCompatActivity activity) {
+            super(activity);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            // Devuelve el fragment correspondiente a la posición
+            switch (position) {
+                case 0:
+                    return new PokedexFragment();
+                case 1:
+                    return new TrainerFragment();
+                case 2:
+                    return new ShopFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            // Devuelve el número total de fragments
+            return 3;
+        }
     }
 }
